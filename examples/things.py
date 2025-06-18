569ce72d716edf2ca13c65a3069a7467638f9d12
Joonas Joensuu <joonas.joensuu@gmail.com>
2025-06-18 22:59:55 +0300

    example jsonified

--- examples/things.py
+++ examples/things.py
@@ -13,13 +13,11 @@ class ThingsResource:
     def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
-        resp.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
-        resp.text = (
-            '\nTwo things awe me most, the starry sky '
-            'above me and the moral law within me.\n'
-            '\n'
-            '    ~ Immanuel Kant\n\n'
-        )
+        # resp.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
+        resp.media = {
+            'quote': 'Two things awe me most, the starry sky above me and the moral law within me.',
+            'author':'Immanuel Kant'
+        }
 
 
 # falcon.App instances are callable WSGI apps
